7f29305daf6c4751011eeee735421bbe7f1f3c2e
Sxmuel999 <samualbuquerque2004@gmail.com>
2023-08-15 11:24:43 -0300

    15 08 2023

---
+++ Calculadora IMC/Calculadora IMC/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_IMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float peso, altura;
+
+            Console.WriteLine("Defina Se é Acima do Peso, SobrePeso ou Peso Normal");
+
+            Console.WriteLine("Me Diga o Seu Peso:");
+            peso = float.Parse(Console.ReadLine());
+
+           
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
