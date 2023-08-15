bb7027fd72bcd490c06b82a0e3748494ba605322
Sxmuel999 <samualbuquerque2004@gmail.com>
2023-08-15 09:40:52 -0300

    Verificar Idade

---
+++ Par ou Impar/Par ou Impar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Par_ou_Ímpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float valor
+
+        }
+    }
+}
---
+++ Par ou Ímpar/Par ou Ímpar/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Par_ou_Ímpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float valor;
+
+            Console.WriteLine("Qual a sua idade?");
+            valor = float.Parse(Console.ReadLine());
+
+            if (valor >=18)
+            {
+                Console.WriteLine("Maior de Idade!");
+            }
+            if (valor < 18)
+            {
+                Console.WriteLine("Menor de idade");
+            }
+        }
+    }
+}
