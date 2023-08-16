7114d31bdffac701d165087b33def44562137008
Sxmuel999 <samualbuquerque2004@gmail.com>
2023-08-16 09:39:11 -0300

    Verificação de Ano Bissexto

---
+++ Ano Bissexto/Ano Bissexto/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ano_Bissexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float ano;
+
+            Console.WriteLine("Selecione um Ano");
+
+            ano = float.Parse(Console.ReadLine());
+
+            if (ano % 4 == 0)
+            {
+                if (ano % 100 != 0)
+                {
+
+                    Console.WriteLine("Bissexto");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não é Bissexto!");
+            }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+                Console.ReadKey();
+            }
+        }
+    }
